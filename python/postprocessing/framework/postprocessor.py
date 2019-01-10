0f09c35d48b64e1e10ff7a80021c270770c671cc
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-01-10 14:55:56 +0100

    added option to run on max events

--- python/postprocessing/framework/postprocessor.py
+++ python/postprocessing/framework/postprocessor.py
@@ -12,7 +12,7 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.jobreport import JobRepo
 
 class PostProcessor :
     def __init__(self,outputDir,inputFiles,cut=None,branchsel=None,modules=[],compression="LZMA:9",friend=False,postfix=None,
-		 jsonInput=None,noOut=False,justcount=False,provenance=False,haddFileName=None,fwkJobReport=False,histFileName=None,histDirName=None, outputbranchsel=None):
+		 jsonInput=None,noOut=False,justcount=False,provenance=False,haddFileName=None,fwkJobReport=False,histFileName=None,histDirName=None, outputbranchsel=None, maxEvents=-1):
 	self.outputDir=outputDir
 	self.inputFiles=inputFiles
 	self.cut=cut
@@ -28,17 +28,18 @@ class PostProcessor :
 	self.haddFileName=haddFileName
 	self.histFile = None
 	self.histDirName = None
+	self.maxEvents = maxEvents
 	if self.jobReport and not self.haddFileName :
 		print "Because you requested a FJR we assume you want the final hadd. No name specified for the output file, will use tree.root"
 		self.haddFileName="tree.root"
- 	self.branchsel = BranchSelection(branchsel) if branchsel else None 
+	self.branchsel = BranchSelection(branchsel) if branchsel else None
         self.outputbranchsel = BranchSelection(outputbranchsel) if outputbranchsel else None
         self.histFileName=histFileName
         self.histDirName=histDirName
     def run(self) :
         outpostfix = self.postfix if self.postfix != None else ("_Friend" if self.friend else "_Skim")
     	if not self.noOut:
-            
+
             if self.compression != "none":
                 ROOT.gInterpreter.ProcessLine("#include <Compression.h>")
                 (algo, level) = self.compression.split(":")
@@ -47,7 +48,7 @@ class PostProcessor :
                 elif algo == "ZLIB": compressionAlgo  = ROOT.ROOT.kZLIB
                 else: raise RuntimeError("Unsupported compression %s" % algo)
             else:
-                compressionLevel = 0 
+                compressionLevel = 0
 	    print "Will write selected trees to "+self.outputDir
             if not self.justcount:
                 if not os.path.exists(self.outputDir):
@@ -56,10 +57,10 @@ class PostProcessor :
             compressionLevel = 0
 
 	if self.noOut:
-	    if len(self.modules) == 0: 
+	    if len(self.modules) == 0:
 		raise RuntimeError("Running with --noout and no modules does nothing!")
 
-        # Open histogram file, if desired 
+        # Open histogram file, if desired
         if (self.histFileName != None and self.histDirName == None) or (self.histFileName == None and self.histDirName != None) :
             raise RuntimeError("Must specify both histogram file and histogram directory!")
         elif self.histFileName != None and self.histDirName != None:
@@ -67,7 +68,7 @@ class PostProcessor :
         else :
             self.histFile = None
 
-    
+
         for m in self.modules:
             if hasattr( m, 'writeHistFile') and m.writeHistFile :
                 m.beginJob(histFile=self.histFile,histDirName=self.histDirName)
@@ -93,20 +94,20 @@ class PostProcessor :
 		continue
 	    else:
 		print 'Pre-select %d entries out of %s '%(elist.GetN() if elist else inTree.GetEntries(),inTree.GetEntries())
-		
+
 	    if fullClone:
 		# no need of a reader (no event loop), but set up the elist if available
 		if elist: inTree.SetEntryList(elist)
 	    else:
 		# initialize reader
-		inTree = InputTree(inTree, elist) 
+		inTree = InputTree(inTree, elist)
 
 	    # prepare output file
             if not self.noOut:
                 outFileName = os.path.join(self.outputDir, os.path.basename(fname).replace(".root",outpostfix+".root"))
                 outFile = ROOT.TFile.Open(outFileName, "RECREATE", "", compressionLevel)
                 outFileNames.append(outFileName)
-                if compressionLevel: 
+                if compressionLevel:
                     outFile.SetCompressionAlgorithm(compressionAlgo)
                 # prepare output tree
                 if self.friend:
@@ -121,27 +122,28 @@ class PostProcessor :
                         fullClone=fullClone,
                         jsonFilter=jsonFilter,
                         provenance=self.provenance)
-            else : 
+            else :
                 outFile = None
                 outTree = None
 
 	    # process events, if needed
 	    if not fullClone:
-		(nall, npass, timeLoop) = eventLoop(self.modules, inFile, outFile, inTree, outTree)
-		print 'Processed %d preselected entries from %s (%s entries). Finally selected %d entries' % (nall, fname, inTree.GetEntries(), npass)
+                print 'if you see me, it is good'
+                (nall, npass, timeLoop) = eventLoop(self.modules, inFile, outFile, inTree, outTree, self.maxEvents)
+                print 'Processed %d preselected entries from %s (%s entries). Finally selected %d entries' % (nall, fname, inTree.GetEntries(), npass)
 	    else:
-		print 'Selected %d entries from %s' % (outTree.tree().GetEntries(), fname)
+                print 'Selected %d entries from %s' % (outTree.tree().GetEntries(), fname)
 
 	    # now write the output
-            if not self.noOut: 
+            if not self.noOut:
                 outTree.write()
                 outFile.Close()
                 print "Done %s" % outFileName
 	    if self.jobReport:
 		self.jobReport.addInputFile(fname,nall)
-		
+
 	for m in self.modules: m.endJob()
-	
+
 	print  totEntriesRead/(time.clock()-t0), "Hz"
 
 
