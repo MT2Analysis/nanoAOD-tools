f8b5ffa02d576a149b5026d3b335614cb7d57aec
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2019-03-06 18:11:24 +0100

    work-in-progress implementation of T1-MET correction

--- python/postprocessing/modules/jme/JetReCalibrator.py
+++ python/postprocessing/modules/jme/JetReCalibrator.py
@@ -69,18 +69,56 @@ class JetReCalibrator:
         if delta != 0:
             if not self.JetUncertainty: raise RuntimeError("Jet energy scale uncertainty shifts requested, but not available")
             self.JetUncertainty.setJetEta(jet.eta())
-            self.JetUncertainty.setJetPt(corr * jet.pt() * jet.rawFactor())
+            self.JetUncertainty.setJetPt(corr * jet.pt() * (1-jet.rawFactor()))
             try:
                 jet.jetEnergyCorrUncertainty = self.JetUncertainty.getUncertainty(True) 
             except RuntimeError as r:
-                print "Caught %s when getting uncertainty for jet of pt %.1f, eta %.2f\n" % (r,corr * jet.pt() * jet.rawFactor(),jet.eta())
+                print "Caught %s when getting uncertainty for jet of pt %.1f, eta %.2f\n" % (r,corr * jet.pt() * (1-jet.rawFactor()),jet.eta())
                 jet.jetEnergyCorrUncertainty = 0.5
             #print "   jet with corr pt %6.2f has an uncertainty %.2f " % (jet.pt()*jet.rawFactor()*corr, jet.jetEnergyCorrUncertainty)
             corr *= max(0, 1+delta*jet.jetEnergyCorrUncertainty)
+        #print 'orig_pt={}, raw_factor={}, raw_pt={}, rho={}, corr_factor={}, final_pt={}'.format(jet.pt, (1.-jet.rawFactor), jet.pt*(1.-jet.rawFactor), rho, corr, jet.pt*(1.-jet.rawFactor)*corr)
         return corr
 
 
-    def correct(self,jet,rho,delta=0,addCorr=False,addShifts=False, metShift=[0,0]):
+    def rawP4forType1MET_(self, jet, muons):
+        """Return the raw 4-vector, after subtracting the muons (if requested),
+           or None if the jet fails the EMF cut."""
+        p4 = jet.p4() * (1-jet.rawFactor) # note different definition of rawfactor wrt heppy
+        #muons = Collection(event, "Muon")
+        #if not jet.hasPFSpecific():
+            # return raw 4-vector if there are no PF details (e.g. AK8 jet below threshold) Not possible on nanoAOD
+        #    return p4
+        #emf = ( jet.physObj.neutralEmEnergy() + jet.physObj.chargedEmEnergy() )/p4.E()
+        #print 'debug in rawP4forType1MET_'
+        emf = (jet.neEmEF + jet.chEmEF) / p4.E()
+        if emf > self.type1METParams['skipEMfractionThreshold']:
+            return None
+        if self.type1METParams['skipMuons']:
+            #print 'debug if skipMuons'
+            #for idau in xrange(jet.numberOfDaughters()):
+            #    pfcand = jet.daughter(idau)
+            #    if pfcand.isGlobalMuon() or pfcand.isStandAloneMuon(): 
+            #        p4 -= pfcand.p4()
+            # NOTE: above cannot be implemented in nanoAOD 
+            # -> check instead overlap with available muons
+            if jet.muonIdx1!=-1:
+              overlap_mu = muons[jet.muonIdx1]
+              if overlap_mu.isGlobal or not overlap_mu.isTracker: # equivalent to global || standalone
+            #    print 'going to subtract first muon' 
+                p4 = - overlap_mu.p4()
+            if jet.muonIdx2!=-1:
+              overlap_mu = muons[jet.muonIdx2]
+              if overlap_mu.isGlobal or not overlap_mu.isTracker: # equivalent to global || standalone
+            #    print 'going to subtract second muon' 
+                p4 = - overlap_mu.p4()
+        #print p4.Pt()
+        return p4
+
+
+
+# TODO: add metShift, and T1 met correction business
+    def correct(self,jet,muons,rho,delta=0,addCorr=False,addShifts=False, metShift=None, type1METCorr=None):
         """Corrects a jet energy (optionally shifting it also by delta times the JEC uncertainty)
 
            If addCorr, set jet.corr to the correction.
@@ -92,12 +130,37 @@ class JetReCalibrator:
            This shift can be applied on top of the *OLD TYPE1 MET*, but only if there was no change 
            in the L1 corrections nor in the definition of the type1 MET (e.g. jet pt cuts).
 
+           The type1METCorr vector, will accumulate the x, y, sumEt type1 MET corrections, to be
+           applied to the *RAW MET*
         """
         raw = 1.-jet.rawFactor  
         corr = self.getCorrection(jet,rho,delta)
         if corr <= 0:
             return jet.pt
         newpt = jet.pt*raw*corr
+
+        # MET recalibration business
+        if not metShift:
+            metShift = [0,0]
+        if not type1METCorr:
+            type1METCorr = [0,0]
+        if newpt > self.type1METParams['jetPtThreshold']:
+            rawP4forT1 = self.rawP4forType1MET_(jet,muons)
+            if rawP4forT1 and rawP4forT1.Pt()*corr > self.type1METParams['jetPtThreshold']:
+                metShift[0] -= rawP4forT1.Px() * (corr - 1.0/raw)
+                metShift[1] -= rawP4forT1.Py() * (corr - 1.0/raw)
+                if self.calculateType1METCorr:
+                    l1corr = self.getCorrection(jet,rho,delta=0,corrector=self.separateJetCorrectors["L1"])
+                    print 'debug corr={}, L1corr={}, rawP4forT1.Px()={}, rawP4forT1.Py()={}'.format(corr, l1corr, rawP4forT1.Px(), rawP4forT1.Py())
+                    #print "\tfor jet with raw pt %.5g, eta %.5g, dpx = %.5g, dpy = %.5g" % (
+                    #            jet.pt()*raw, jet.eta(), 
+                    #            rawP4forT1.Px()*(corr - l1corr), 
+                    #            rawP4forT1.Py()*(corr - l1corr))
+                    type1METCorr[0] -= rawP4forT1.Px() * (corr - l1corr) 
+                    type1METCorr[1] -= rawP4forT1.Py() * (corr - l1corr) 
+                    #type1METCorr[2] += rawP4forT1.Et() * (corr - l1corr) 
+        print 'orig_pt={}, raw_factor={}, raw_pt={}, rho={}, corr_factor={}, final_pt={}'.format(jet.pt, raw, jet.pt*raw, rho, corr, jet.pt*raw*corr)
+        print 'debug, metShiftX={}, metShiftY={}, type1METCorrX={}, type1METCorrY={}'.format(metShift[0],metShift[1],type1METCorr[0],type1METCorr[1])
         return newpt
 
 
--- python/postprocessing/modules/jme/jetRecalib.py
+++ python/postprocessing/modules/jme/jetRecalib.py
@@ -1,3 +1,6 @@
+# Module to propagate to the MET calculation the re-calibration of the jets:
+# MET is Type-1 PF MET and Type-1 corrections are re-calculated following criteria at https://twiki.cern.ch/twiki/bin/view/CMS/METType1Type2Formulae#3_The_Type_I_correction
+
 import ROOT
 import math, os,re
 import numpy as np
@@ -20,13 +23,15 @@ class jetRecalib(Module):
             raise ValueError("ERROR: Invalid jet type = '%s'!" % jetType)
         self.rhoBranchName = "fixedGridRhoFastjetAll"
         self.lenVar = "n" + self.jetBranchName
+        # TODO: these lines are useless, please remove
         # To do : change to real values
         self.jmsVals = [1.00, 0.99, 1.01]
         
-
         self.jesInputFilePath = os.environ['CMSSW_BASE'] + "/src/PhysicsTools/NanoAODTools/data/jme/"
 
-        self.jetReCalibrator = JetReCalibrator(globalTag, jetType , True, self.jesInputFilePath, calculateSeparateCorrections = False, calculateType1METCorrection  = False)
+        type1METParams={'jetPtThreshold':15., 'skipEMfractionThreshold':0.9, 'skipMuons':True} 
+        #self.jetReCalibrator = JetReCalibrator(globalTag, jetType, True, self.jesInputFilePath, calculateSeparateCorrections = False, calculateType1METCorrection  = False)
+        self.jetReCalibrator = JetReCalibrator(globalTag, jetType, True, self.jesInputFilePath, calculateSeparateCorrections=False, calculateType1METCorrection=True, type1METParams=type1METParams)
 	
         # load libraries for accessing JES scale factors and uncertainties from txt files
         for library in [ "libCondFormatsJetMETObjects", "libPhysicsToolsNanoAODTools" ]:
@@ -45,6 +50,10 @@ class jetRecalib(Module):
         self.out.branch("%s_pt_nom" % self.jetBranchName, "F", lenVar=self.lenVar)
         self.out.branch("MET_pt_nom" , "F")
         self.out.branch("MET_phi_nom", "F")
+        self.out.branch("MET_pt_naive" , "F")
+        self.out.branch("MET_phi_naive", "F")
+        self.out.branch("MET_pt_shift" , "F")
+        self.out.branch("MET_phi_shift", "F")
             
                         
     def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
@@ -52,32 +61,69 @@ class jetRecalib(Module):
     
     def analyze(self, event):
         """process event, return True (go to next module) or False (fail, go to next event)"""
-        jets = Collection(event, self.jetBranchName )
-        met = Object(event, "MET") 
 
+        jets = Collection(event, self.jetBranchName )
+        met = Object(event, "MET") # NOTE: Do not use to re-calibrate 2017 data
+        rawmet = Object(event, "RawMET") 
+        muons = Collection(event, "Muon")
+
+        # "_nom" collections and branches will store re-calibrated quantities
         jets_pt_nom = []
-        ( met_px,         met_py         ) = ( met.pt*math.cos(met.phi), met.pt*math.sin(met.phi) )
-        ( met_px_nom, met_py_nom ) = ( met_px, met_py )
-        met_px_nom = met_px
-        met_py_nom = met_py
+        (met_px, met_py) = (met.pt*math.cos(met.phi), met.pt*math.sin(met.phi))
+        (met_px_nom, met_py_nom) = (met_px, met_py)
+        (met_px_naive,met_py_naive) = (met_px, met_py) # a' la stephane
+        #met_px_nom = met_px useless line
+        #met_py_nom = met_py useless line
                 
         rho = getattr(event, self.rhoBranchName)
+        if rho==0 or rho==None: print 'WARNING: rho not correctly set for this event'
+        #print 'rho={}'.format(rho)
+
+        metShift=[0,0] # met correction accumulator
+        type1METCorr=[0,0] # 
         
         for jet in jets:
-	    jet_pt=jet.pt
-	    jet_pt = self.jetReCalibrator.correct(jet,rho)
-            jet_pt_nom           = jet_pt # don't smear resolution in data
+	    #jet_pt = jet.pt
+	    jet_pt = self.jetReCalibrator.correct(jet=jet,muons=muons,rho=rho,delta=0,addCorr=False,addShifts=False, metShift=metShift, type1METCorr=type1METCorr)
+            jet_pt_nom = jet_pt # don't smear resolution in data
             if jet_pt_nom < 0.0:
                 jet_pt_nom *= -1.0
-            jets_pt_nom    .append(jet_pt_nom)
+            jets_pt_nom.append(jet_pt_nom) # concerning jets I am done here ...
+
+            # now comes the met with naive correction
             if jet_pt_nom > 15.:
-                jet_cosPhi = math.cos(jet.phi)
-                jet_sinPhi = math.sin(jet.phi)
-                met_px_nom = met_px_nom - (jet_pt_nom - jet.pt)*jet_cosPhi
-                met_py_nom = met_py_nom - (jet_pt_nom - jet.pt)*jet_sinPhi
+              jet_cosPhi = math.cos(jet.phi)
+              jet_sinPhi = math.sin(jet.phi)
+              met_px_naive = met_px_naive - (jet_pt_nom - jet.pt)*jet_cosPhi
+              met_py_naive = met_py_naive - (jet_pt_nom - jet.pt)*jet_sinPhi
+
+        # at this point x,y components are already summed in metShift, type1METCorr
+        met_px_nom = rawmet.pt*math.cos(rawmet.phi) + type1METCorr[0]
+        met_py_nom = rawmet.pt*math.sin(rawmet.phi) + type1METCorr[1]
+        met_pt_nom = math.sqrt(met_px_nom**2 + met_py_nom**2)
+        met_phi_nom = math.atan2(met_py_nom, met_px_nom) 
+        # 
+        met_px_shift = met_px + metShift[0] 
+        met_py_shift = met_py + metShift[1] 
+        met_pt_shift = math.sqrt(met_px_shift**2 + met_py_shift**2)
+        met_phi_shift = math.atan2(met_py_shift, met_px_shift) 
+
+        met_pt_naive = math.sqrt(met_px_naive**2 + met_py_naive**2)
+        met_phi_naive = math.atan2(met_py_naive,met_px_naive)
+        # debug 
+
         self.out.fillBranch("%s_pt_nom" % self.jetBranchName, jets_pt_nom)
-        self.out.fillBranch("MET_pt_nom", math.sqrt(met_px_nom**2 + met_py_nom**2))
-        self.out.fillBranch("MET_phi_nom", math.atan2(met_py_nom, met_px_nom))        
+        self.out.fillBranch("MET_pt_nom", met_pt_nom)
+        self.out.fillBranch("MET_phi_nom", met_phi_nom)        
+        self.out.fillBranch("MET_pt_naive", met_pt_naive)
+        self.out.fillBranch("MET_phi_naive", met_phi_naive)        
+        self.out.fillBranch("MET_pt_shift", met_pt_shift)
+        self.out.fillBranch("MET_phi_shift", met_pt_shift)        
+
+################ my stuff to debug        
+
+        #print 'orig_met_pt={:.1f}, orig_met_phi={:.2f}, nom_met_pt={:.1f} nom_met_phi={:.2f}, shift_met_pt={:.1f}, shift_met_phi={:.1f}'.format(met.pt, met.phi, met_pt_nom, met_phi_nom, met_pt_shift, met_phi_shift) 
+        print 'orig_met_pt={:.1f}, orig_met_phi={:.2f}, raw_met_pt={:.1f}, raw_met_phi={:.2f} nom_met_pt={:.1f} nom_met_phi={:.2f}'.format(met.pt, met.phi, rawmet.pt, rawmet.phi, met_pt_nom, met_phi_nom) 
 
         return True
 
